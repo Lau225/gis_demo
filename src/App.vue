df88c533fd6339ac6077631493cf1b30acbf70b1
刘1300 <8641121+liu-1300@user.noreply.gitee.com>
2025-04-17 08:58:21 +0800

    设置地图经纬度和坐标

--- src/App.vue
+++ src/App.vue
@@ -26,31 +26,42 @@ const city = ref([])
 const city_id = ref('0')
 const area_id = ref('0')
 const areaList = ref([])
+const areaLocation = ref('')
 const getCityList = async () => {
-    area_id.value = '0'
-    const res = await $get("/api/aj/getcitycode")
-    city.value = res.result.hotcity
+  area_id.value = '0'
+  const res = await $get("/api/aj/getcitycode")
+  city.value = res.result.hotcity
 }
 
 const changeArea = async () => {
-  if(area_id.value !== '0'){
-    const res = await $get("/api/at/shop",{
-      city_id:city_id.value,
-      area_id:area_id.value,
+  let address = city.value.find(c => c.code === city_id.value).name + areaList.value.find(a => a.area_id === area_id.value).area_name
+  // 获取制定的区域的经纬度
+  const res1 = await $get("https://restapi.amap.com/v3/geocode/geo", {
+    key: '3e75af85d89ec018cfb8bf58795888a6',
+    address
+  })
+  areaLocation.value = res1.geocodes[0].location
+
+  // 设置地图的经纬度和坐标
+  map.value.setZoomAndCenter(14, areaLocation.value.split(","))
+
+  if (area_id.value !== '0') {
+    const res = await $get("/api/at/shop", {
+      city_id: city_id.value,
+      area_id: area_id.value,
     })
-    console.log(res)
   }
 }
 
 const changeCity = async () => {
   areaList.value = []
-  const res = await $get('/api/aj/get_area',{
+  const res = await $get('/api/aj/get_area', {
     citycode: city_id.value
   })
   areaList.value = Object.keys(res.result).map(item => {
     return {
-      area_id:item,
-      area_name:res.result[item]
+      area_id: item,
+      area_name: res.result[item]
     }
   })
   areaList.value
